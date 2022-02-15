b8c4ed8ea7fcada25e8fcba1c73de71e33caadf4
Bethany Dunfield <brdunfield@gmail.com>
2022-02-15 11:01:49 -0700

    add manifest

---
+++ public/manifest.js
@@ -0,0 +1,16 @@
+{
+  "name": "Hello Hue",
+  "short_name": "hello hue",
+  "display": "standalone",
+  "orientation": "portrait",
+  "theme_color": "#FFFFFF",
+  "background_color": "#FFFFFF",
+  "start_url": "/",
+  "icons": [
+    {
+      "src": "/icons/icon-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
